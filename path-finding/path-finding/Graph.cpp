5c3f0fd993bd91f793ced2d6fe87fdac63995511
Beight <christian.lindas@hotmail.com>
2018-07-08 20:37:44 +0200

    Refined Code
    Refined the code in graph in node classes.
    - More appropriate variable names
    - Some function local variables are now class member variables to avoid sending them as arguments in private functions
    - Added some more const where appropriate.
    - Update some comments

--- path-finding/path-finding/Graph.cpp
+++ path-finding/path-finding/Graph.cpp
@@ -3,15 +3,15 @@
 #include <deque>
 
 
-Graph::Graph(unsigned int graphWidth, unsigned int graphHeight, unsigned int outBufferSize)
-	: m_graphWidth(graphWidth), m_graphHeight(graphHeight), m_pValue(0.001f)
+Graph::Graph(unsigned int graphRows, unsigned int graphColumns, unsigned int outBufferSize)
+	: m_graphRows(graphRows), m_graphColumns(graphColumns), m_pValue(0.001f)
 {
 	//pValue is used to avoid ties between nodes in their estimated distance.
 	//We use 1/1000 as a standard value but if the max length of the path is longer we use that instead.
 	if (outBufferSize > 1000)
 		m_pValue = 1.0f / static_cast<float>(outBufferSize);
 
-	m_nodes.reserve(m_graphWidth * m_graphHeight);
+	m_nodes.reserve(m_graphRows * m_graphColumns);
 }
 
 Graph::~Graph()
@@ -19,28 +19,28 @@ Graph::~Graph()
 	m_nodes.clear();
 }
 
-void Graph::AddNode(Node node)
+void Graph::AddNode(int nodeIndex, Position nodePosition, bool passable)
 {
-	m_nodes.push_back(node);
+	m_nodes.emplace_back(Node(nodeIndex, nodePosition, passable));
 }
 
 int Graph::FindShortestPath(const Position& start, const Position& target)
 {
-	VisitList nodesToVisit;
+	m_currentTarget = target;
 	//Locate the start node and add it to the visit list.
 	for (unsigned int i = 0; i < m_nodes.size(); i++)
 	{
 		if (m_nodes.at(i).GetPos() == start)
 		{
 			m_nodes.at(i).SetAddedToVisitList(true);
-			nodesToVisit.push(std::make_pair(0.0f, m_nodes.at(i).GetIndex()));
+			m_visitList.push(std::make_pair(0.0f, m_nodes.at(i).GetIndex()));
 			break;
 		}
 	}
 
 	int targetFound = -1;
 	//Begin the search
-	while (!nodesToVisit.empty())
+	while (!m_visitList.empty())
 	{
 		//Get next node to visit
 		bool nextNodeFound = false;
@@ -49,12 +49,12 @@ int Graph::FindShortestPath(const Position& start, const Position& target)
 			//Duplicates are allowed in visitlist but are removed if they have already been visited.
 			//This is compromise since we can't erase entries from the priority queue when we
 			//find an already added node but with a shorter path.
-			if (m_nodes.at(nodesToVisit.top().second).GetVisited())
-				nodesToVisit.pop();
+			if (m_nodes.at(m_visitList.top().second).GetVisited())
+				m_visitList.pop();
 			else
 				nextNodeFound = true;
 		}
-		Node& currentNode = m_nodes.at(nodesToVisit.top().second);
+		Node& currentNode = m_nodes.at(m_visitList.top().second);
 
 		//Check if this node is our target
 		if (currentNode.GetPos() == target)
@@ -64,75 +64,76 @@ int Graph::FindShortestPath(const Position& start, const Position& target)
 		}
 		currentNode.SetVisited(true);
 		//Remove node from visit list.
-		nodesToVisit.pop();
+		m_visitList.pop();
+		int currentIndex = currentNode.GetIndex();
 
 		//Add above neighbour to visit list, if one exists
 		if (currentNode.GetPos().m_nX != 0)
 		{
 			//Check if node is passable and not visited already.
-			Node& n = m_nodes.at(currentNode.GetIndex() - 1);
-			if (n.GetPassable() && !n.GetVisited())
-				AddVisitList(n, currentNode.GetIndex(), currentNode.GetSteps(), nodesToVisit, target);
+			Node& node = m_nodes.at(currentNode.GetIndex() - 1);
+			if (node.GetPassable() && !node.GetVisited())
+				AddVisitList(node, currentIndex, currentNode.GetSteps());
 		}
 		//Add right neighbour to visit list, if one exists
-		if (currentNode.GetPos().m_nY != (m_graphHeight - 1))
+		if (currentNode.GetPos().m_nY != (m_graphColumns - 1))
 		{
 			//Check if node is passable and not visited already.
-			Node& n = m_nodes.at(currentNode.GetIndex() + m_graphWidth);
-			if (n.GetPassable() && !n.GetVisited())
-				AddVisitList(n, currentNode.GetIndex(), currentNode.GetSteps(), nodesToVisit, target);
+			Node& node = m_nodes.at(currentNode.GetIndex() + m_graphRows);
+			if (node.GetPassable() && !node.GetVisited())
+				AddVisitList(node, currentIndex, currentNode.GetSteps());
 		}
 		//Add under neighbour to visit list, if one exists
-		if (currentNode.GetPos().m_nX != (m_graphWidth - 1))
+		if (currentNode.GetPos().m_nX != (m_graphRows - 1))
 		{
 			//Check if node is passable and not visited already.
-			Node& n = m_nodes.at(currentNode.GetIndex() + 1);
-			if (n.GetPassable() && !n.GetVisited())
-				AddVisitList(n, currentNode.GetIndex(), currentNode.GetSteps(), nodesToVisit, target);
+			Node& node = m_nodes.at(currentNode.GetIndex() + 1);
+			if (node.GetPassable() && !node.GetVisited())
+				AddVisitList(node, currentIndex, currentNode.GetSteps());
 		}
 		//Add left neighbour to visit list, if one exists
 		if (currentNode.GetPos().m_nY != 0)
 		{
 			//Check if node is passable and not visited already.
-			Node& n = m_nodes.at(currentNode.GetIndex() - m_graphWidth);
-			if (n.GetPassable() && n.GetVisited())
-				AddVisitList(n, currentNode.GetIndex(), currentNode.GetSteps(), nodesToVisit, target);
+			Node& node = m_nodes.at(currentNode.GetIndex() - m_graphRows);
+			if (node.GetPassable() && node.GetVisited())
+				AddVisitList(node, currentIndex, currentNode.GetSteps());
 		}
 	}
 
 	return targetFound;
 }
 
-void Graph::AddVisitList(Node& n, const unsigned int currentIndex, const int currentSteps, VisitList &nodesToVisit, const Position &target)
+void Graph::AddVisitList(Node& node, const unsigned int currentIndex, const int currentSteps)
 {
 	//Check if node has not been added to the visit list
-	if (!n.GetAddedToVisitList())
+	if (!node.GetAddedToVisitList())
 	{
-		n.SetFromIndex(currentIndex);
+		node.SetFromIndex(currentIndex);
 
-		float estimatedDistance = static_cast<float>(currentSteps + 1) + (n.GetPos().GetDistance(target) * (1.0f + m_pValue));
+		float estimatedDistance = static_cast<float>(currentSteps + 1) + (node.GetPos().GetDistance(m_currentTarget) * (1.0f + m_pValue));
 
-		n.SetEstimatedDistance(estimatedDistance);
-		n.SetSteps(currentSteps + 1);
-		nodesToVisit.push(std::make_pair(estimatedDistance, n.GetIndex()));
-		n.SetAddedToVisitList(true);
+		node.SetEstimatedDistance(estimatedDistance);
+		node.SetSteps(currentSteps + 1);
+		m_visitList.push(std::make_pair(estimatedDistance, node.GetIndex()));
+		node.SetAddedToVisitList(true);
 		return;
 	}
-	//node already in visit list with lower distance
-	else if ((currentSteps + 1) >= n.GetSteps())
+	//node already in visit list with lower distance, don't add it to visit list.
+	else if ((currentSteps + 1) >= node.GetSteps())
 		return;
 
 	//This is a shorter path to this node than before.
-	n.SetFromIndex(currentIndex);
-	n.SetSteps(currentSteps + 1);
+	node.SetFromIndex(currentIndex);
+	node.SetSteps(currentSteps + 1);
 
-	float estimatedDistance = static_cast<float>(currentSteps + 1) + (n.GetPos().GetDistance(target) * (1.0f + m_pValue));
+	float estimatedDistance = static_cast<float>(currentSteps + 1) + (node.GetPos().GetDistance(m_currentTarget) * (1.0f + m_pValue));
 
-	n.SetEstimatedDistance(estimatedDistance);
-	nodesToVisit.push(std::make_pair(estimatedDistance, n.GetIndex()));
+	node.SetEstimatedDistance(estimatedDistance);
+	m_visitList.push(std::make_pair(estimatedDistance, node.GetIndex()));
 }
 
-void Graph::ConstructPath(Node node, int *pOutBuffer, const int nOutBufferSize)
+void Graph::ConstructPath(const Node& node, int* outBuffer, const int outBufferSize)
 {
 	std::deque<int> path;
 	path.push_front(node.GetIndex());
@@ -145,14 +146,14 @@ void Graph::ConstructPath(Node node, int *pOutBuffer, const int nOutBufferSize)
 	}
 	for (unsigned int i = 0; i < path.size(); i++)
 	{
-		if (static_cast<int>(i) < nOutBufferSize)
-			pOutBuffer[i] = path.at(i);
+		if (static_cast<int>(i) < outBufferSize)
+			outBuffer[i] = path.at(i);
 		else
 			break;
 	}
 }
 
-Node Graph::GetNode(unsigned int nodeIndex)
+const Node& Graph::GetNode(const unsigned int nodeIndex) const
 {
 	return m_nodes.at(nodeIndex);
 }
